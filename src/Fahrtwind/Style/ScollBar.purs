0731ead74031371fe11ccc4458907a1416814a7c
Mark Eibes <mark.eibes@gmail.com>
2022-06-23 18:40:55 +0200

    Add scrollBar'

--- src/Fahrtwind/Style/ScollBar.purs
+++ src/Fahrtwind/Style/ScollBar.purs
@@ -3,17 +3,18 @@ module Fahrtwind.Style.ScollBar where
 import Prelude
 
 import Color (Color, cssStringRGBA)
-import React.Basic.Emotion (Style, css, nested, str)
+import React.Basic.Emotion (Style, StyleProperty, css, nested, str)
 import React.Basic.Emotion as E
+import Unsafe.Coerce (unsafeCoerce)
 
-scrollBar
-  :: { background :: Color
-     , col :: Color
-     , width :: Int
-     , borderRadius :: Int
-     , borderWidth :: Int
-     }
-  -> Style
+scrollBar ∷
+  { background ∷ Color
+  , col ∷ Color
+  , width ∷ Int
+  , borderRadius ∷ Int
+  , borderWidth ∷ Int
+  } →
+  Style
 scrollBar { background, col, width, borderRadius, borderWidth } = css
   { -- Firefox
     scrollbarWidth: E.auto
@@ -29,6 +30,30 @@ scrollBar { background, col, width, borderRadius, borderWidth } = css
   , "&::-webkit-scrollbar-thumb": nested $ css
       { background: str $ cssStringRGBA col
       , borderRadius: str $ show borderRadius <> "px"
-      , border: str $ show borderWidth <> "px solid " <> cssStringRGBA background
+      , border: str $ show borderWidth <> "px solid " <> cssStringRGBA
+          background
+      }
+  }
+
+scrollBar' ∷
+  { background ∷ StyleProperty
+  , col ∷ StyleProperty
+  , width ∷ StyleProperty
+  , borderRadius ∷ StyleProperty
+  , borderWidth ∷ StyleProperty
+  } →
+  Style
+scrollBar' { background, col, width, borderRadius, borderWidth } = css
+  { -- Firefox
+    scrollbarWidth: E.auto
+  , scrollbarColor: str (unsafeCoerce col <> " " <> unsafeCoerce background)
+  -- Chrome, Edge, and Safari
+  , "&::-webkit-scrollbar": nested $ css { width }
+  , "&::-webkit-scrollbar-track": nested $ css { background }
+  , "&::-webkit-scrollbar-thumb": nested $ css
+      { background: col
+      , borderRadius
+      , border: str
+          (unsafeCoerce borderWidth <> " solid " <> unsafeCoerce background)
       }
   }
